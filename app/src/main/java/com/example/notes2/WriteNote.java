52e30a540119b5ef4f84cbc7b0ebde93c73eeb07
Ras <x.suwaidi@gmail.com>
2017-07-27 21:20:13 +0100

    read/write from one message

--- app/src/main/java/com/example/notes2/WriteNote.java
+++ app/src/main/java/com/example/notes2/WriteNote.java
@@ -12,8 +12,15 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.nio.file.Files;
 
 import static com.example.notes2.R.id.content_write;
 
@@ -27,6 +34,8 @@ public class WriteNote extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        EditText journal = (EditText) findViewById(content_write);
+        journal.append(readFromFile(this));
 
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -38,15 +47,10 @@ public class WriteNote extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
-
-    @Override
-    public void onBackPressed() {
-        Toast.makeText(this,"Thanks for using application!!", Toast.LENGTH_LONG).show();
-        finish();
 
 
-        return;
+
+
     }
 
     private void writeToFile(String data,Context context) {
@@ -58,6 +62,54 @@ public class WriteNote extends AppCompatActivity {
         catch (IOException e) {
             Log.e("Exception", "File write failed: " + e.toString());
         }
+    }
+
+
+    private String readFromFile(Context context) {
+
+        String ret = "";
+
+        try {
+            InputStream inputStream = context.openFileInput("config.txt");
+
+            if ( inputStream != null ) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                    stringBuilder.append(receiveString);
+                }
+
+                inputStream.close();
+                ret = stringBuilder.toString();
+            }
+        }
+        catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        }
+
+        return ret;
+    }
+
+
+
+    @Override
+    public void onBackPressed() {
+        Toast.makeText(this,"Thanks for using application!!", Toast.LENGTH_LONG).show();
+        finish();
+
+        EditText journal = (EditText) findViewById(content_write);
+        String journalText = journal.getText().toString();
+
+
+        writeToFile(journalText, this);
+
+
+        return;
     }
 
 }
